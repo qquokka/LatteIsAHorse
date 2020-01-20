96b52dcf154ec40d2e2a97b319ae866ba7c715ca
dodonmountain <lkh151515@gmail.com>
2020-01-20 15:13:51 +0900

    KH | set vuex && vue-router

---
+++ Frontend/src/components/LoginForm.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <h2 class="text-success font-weight-bolder">HaPPY <span class="text-info">LoGIN</span></h2>
+        <form class="col-5 mx-auto p-4 shadow-lg border border-top-0" @submit.prevent="login">
+            <div class="form-group">
+                <input type="text" class="form-control" id="inputId" placeholder="username" v-model="credentials.username">
+                <small id="idHelp" class="form-text text-muted">가입하실 때 작성하신 사용자 이름을 입력하세요.</small>
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" id="inputPW" placeholder="password" v-model="credentials.password">
+            </div>
+            <button type="submit" class="btn btn-outline-warning shadow">로그인</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                credentials: {
+                }
+            }
+        },
+        methods: {
+            loginLog() {
+                console.log('함수가 실행되었습니다!!!!!!!!!!!!!!')
+                console.log(this.id)
+                console.log(this.password)
+            },
+            login() {
+                console.log('emit')
+                this.$emit('login-event', this.credentials)
+                this.credentials = {}
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,18 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueYouTubeEmbed from 'vue-youtube-embed'
-Vue.use(VueYouTubeEmbed)
-// if you don't want install the component globally
-Vue.use(VueYouTubeEmbed, { global: false })
-// if you want to install the component globally with a different name
-Vue.use(VueYouTubeEmbed, { global: true, componentId: "youtube-media" })
-// for bootstrap
+import router from './router'
 import 'expose-loader?$!expose-loader?jQuery!jquery'
 import 'popper.js/dist/popper.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import VueSession from 'vue-session'
+import store from './store' // vuex
+
+
 Vue.config.productionTip = false
+Vue.use(VueSession)
 
 new Vue({
-  render: h => h(App),
+  router,
+  store, // vuex
+  render: h => h(App)
 }).$mount('#app')
