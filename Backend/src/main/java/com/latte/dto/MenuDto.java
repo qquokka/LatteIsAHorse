c041b2621282a0f82f8b7649b251a87f3d24430f
SiHo Ryu <ujakifox2@gmail.com>
2020-01-31 09:10:45 +0900

    SH | minor modification backend

--- Backend/src/main/java/com/latte/dto/MenuDto.java
+++ Backend/src/main/java/com/latte/dto/MenuDto.java
@@ -8,10 +8,11 @@ public class MenuDto {
 	private String description;
 	private int cafe_id;
 	private String tag;
+
 	public MenuDto() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
+
 	public MenuDto(int mid, String product, int price, String description, int cafe_id, String tag) {
 		super();
 		this.mid = mid;
@@ -21,11 +22,13 @@ public class MenuDto {
 		this.cafe_id = cafe_id;
 		this.tag = tag;
 	}
+
 	@Override
 	public String toString() {
 		return "MenuDto [mid=" + mid + ", product=" + product + ", price=" + price + ", description=" + description
 				+ ", cafe_id=" + cafe_id + ", tag=" + tag + "]";
 	}
+
 	public int getMid() {
 		return mid;
 	}
@@ -33,21 +36,27 @@ public class MenuDto {
 	public String getProduct() {
 		return product;
 	}
+
 	public void setProduct(String product) {
 		this.product = product;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getCafe_id() {
 		return cafe_id;
 	}
@@ -55,10 +64,9 @@ public class MenuDto {
 	public String getTag() {
 		return tag;
 	}
+
 	public void setTag(String tag) {
 		this.tag = tag;
 	}
-	
-	
 
 }
