33587f8820385c457588cbf54aaae7d08ab3608d
jinwon Hyun <94epicenter@gmail.com>
2020-02-03 17:30:32 +0900

    jw | Coupon implements

---
+++ Backend/src/main/java/com/latte/dao/CouponDaoImpl.java
@@ -0,0 +1,49 @@
+
+package com.latte.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.latte.dto.Coupon;
+
+@Repository
+public class CouponDaoImpl {
+	private final String ns = "mapper.coupon.";
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	// DB 에 저장된 모든 쿠폰을 반환[관리자용]
+	public List<Coupon> getCouponList() {
+		return sqlSession.selectList(ns + "getCouponList");
+	}
+
+	// 카페아이디로 등록된 쿠폰을 보여줍니다.
+	public List<Coupon> getCouponListByCafeId(Long cafe_id) {
+		return sqlSession.selectList(ns + "getCouponListByCafeId", cafe_id);
+	}
+
+	// Create
+	public int addCoupon(Coupon coupon) {
+		return sqlSession.insert(ns + "addCoupon", coupon);
+	}
+
+	// Read
+	public Coupon getCouponByUserId(Long user_id) {
+		return sqlSession.selectOne(ns + "getCouponByUserId", user_id);
+	}
+
+	// Update
+	public int updateCoupon(Coupon coupon) {
+		return sqlSession.update(ns + "updateCoupon", coupon);
+	}
+
+	// Delete
+	public int deleteCouponByUserId(int user_id) {
+		return sqlSession.delete(ns + "deleteCouponByUserId", user_id);
+	}
+
+}
--- Backend/src/main/java/com/latte/dto/Coupon.java
+++ Backend/src/main/java/com/latte/dto/Coupon.java
@@ -2,9 +2,52 @@ package com.latte.dto;
 
 import java.io.Serializable;
 
-public class Coupon implements Serializable{
+public class Coupon implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private Long id; // bigint(20) NOT NULL AUTO_INCREMENT,
+	private Long users_id;
+	private Long cafe_id;
+	private Integer count;
 
-	
+	public Coupon() {
+		super();
+	}
+
+	public Coupon(Long id, Long users_id, Long cafe_id, Integer count) {
+		super();
+		this.id = id;
+		this.users_id = users_id;
+		this.cafe_id = cafe_id;
+		this.count = count;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Long getUsers_id() {
+		return users_id;
+	}
+
+	public void setUsers_id(Long users_id) {
+		this.users_id = users_id;
+	}
+
+	public Long getCafe_id() {
+		return cafe_id;
+	}
+
+	public void setCafe_id(Long cafe_id) {
+		this.cafe_id = cafe_id;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
 }
