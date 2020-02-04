ad85ac946529c4b3ea4a2c22784af363f084f1f4
HyejinYang <yang94lol@naver.com>
2020-02-04 14:33:40 +0900

    HJ | Created Cafe Page

--- Frontend/src/views/section/PostList.vue
+++ Frontend/src/views/section/PostList.vue
@@ -38,12 +38,6 @@ export default {
     loadmore() {
       this.limits += 6  
     },
-    // deletePost(postId) {
-    //   console.log('삭제시작')
-    //   axios.delete(`${this.$store.state.constants.SERVER}/post/${postId}`, {headers: {'Authorization': "Bearer " + this.$session.get('jwt')}})
-    //       .then(response => {
-    //         console.log(response)
-    //       })
   }
 }
 </script>
