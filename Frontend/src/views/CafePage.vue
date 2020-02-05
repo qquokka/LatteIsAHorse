93adfc8bd0dbc881cdac473f3d27605ff7eb2390
HyejinYang <yang94lol@naver.com>
2020-02-05 11:38:53 +0900

    Display if a cafe is open (incomplete)

--- Frontend/src/views/CafePage.vue
+++ Frontend/src/views/CafePage.vue
@@ -2,6 +2,8 @@
 	<div class="container-fluid px-3">
 		<img :src="info.thumbnail" :alt="info.cafe_name">
 		<h1>{{ info.cafe_name }}</h1>
+		<button v-if="isOpen" class="btn btn-primary">영업중</button>
+		<button v-else class="btn btn-secondary">준비중</button>
 		<hr>
 		<p>좋아요: {{ info.like_count }}</p>
 		<p>태그: {{ info.tag }}</p>
@@ -34,7 +36,7 @@
 						<h5 class="card-title">{{ review.title }}</h5>
 					</div>
 					<ul class="list-group list-group-flush">
-						<li class="list-group-item"><p class="my-0">{{ review.created_at }} 작성 | {{ review.updated_at }} 수정</p></li>
+						<li class="list-group-item"><p class="my-0">{{ review.updated_at }} 작성</p></li>
 						<li class="list-group-item" v-html="review.content"></li>
 					</ul>
 				</div>
@@ -45,7 +47,7 @@
 
 <script>
 import axios from 'axios'
-import moment from 'moment'; moment.locale('kr');
+import moment from 'moment'
 
 export default {
 	name: 'CafePage',
@@ -57,7 +59,7 @@ export default {
 			info: {},
 			menus: [],
 			reviews: [],
-			isOpen: true
+			isOpen: false
 		}
 	},
 	methods: {
@@ -69,10 +71,39 @@ export default {
 						this.reviews = response.data.post
 						this.menus = response.data.menu
 						
+						// 리뷰 작성시간이 12시간 이내이면 '3시간 전' 이런 식으로 나오게 하고, 12시간 이전이면 날짜 시간 다 표시
+						let now = Date.now()
 						this.reviews.forEach(review => {
-							review.created_at = review.created_at.replace(/T|Z/g, " ")
-							review.updated_at = review.updated_at.replace(/T|Z/g, " ")
+							let date = new Date(review.updated_at)
+							if (now - Date.parse(date) <= 43200000) {
+								review.updated_at = moment(review.updated_at).locale('ko').fromNow()
+							} else {
+								review.updated_at = moment(review.updated_at).locale('ko').format('llll')
+							}
 						})
+
+						now = new Date(now)
+						let day = now.getDay()
+						// console.log(day, response.data.time[day][0], response.data.time[day][1])
+						let openTime = new Date(response.data.time[day][0])
+						let closeTime = new Date(response.data.time[day][1])
+						// console.log(openTime.getDate(), closeTime.getDate())
+						let nowTime = now.getHours() * 100 + now.getMinutes()
+
+						let closeHour = closeTime.getHours()
+						// 새벽 1시에 끝나면 25시에 끝난다고 생각
+						if (openTime.getDate() !== closeTime.getDate()) {
+							closeHour += 24
+						}
+						if (openTime.getHours() * 100 + openTime.getMinutes() <= nowTime && nowTime < closeHour * 100 + closeTime.getMinutes()) {
+							this.isOpen = true
+						} else {
+							this.isOpen = false
+						}
+						
+						console.log(openTime.getHours() * 100 + openTime.getMinutes())
+						console.log(nowTime)
+						console.log(this.isOpen)
 					})
 					.catch(error => {
 						console.log(error.data)
