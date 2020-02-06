e690613a6bc583589b5d371ddb43b966341f629b
johh201200 <johh201200@gmail.com>
2020-02-06 14:11:18 +0900

    hh | userslikecafe

--- Backend/src/main/java/com/latte/service/IUsersLikeCafeService.java
+++ Backend/src/main/java/com/latte/service/IUsersLikeCafeService.java
@@ -2,25 +2,19 @@ package com.latte.service;
 
 import java.util.List;
 
-import com.latte.UsersLikeCafedto.UsersLikeCafe;
+import com.latte.dto.UsersLikeCafeDto;
 
 public interface IUsersLikeCafeService {
-	// DB 에 저장된 모든 좋아요를 반환[관리자용]
-	List<UsersLikeCafe> getLikeList();
+	// user가 좋아요를 누른 모든 카페 반환
+	List<UsersLikeCafeDto> getUsersLikeCafeList();
 
-	// 
-	List<UsersLikeCafe> getLikeListByUserId(int user_id);
+	// 카페의 전체 좋아요 수를 반환
+	List<UsersLikeCafeDto> getUsersLikeCafeByCafeId(int cafe_id);
 
-	// Create
-	int addLike(UsersLikeCafe like);
+	// 좋아요 누름
+	int addUsersLikeCafe(UsersLikeCafeDto users_id);
 
-	// READ
-	UsersLikeCafe getLikeByUserId(Long user_id);
-
-	// Update
-	int updateLike(UsersLikeCafe like);
-
-	// Delete
-	int deleteLikeByUserId(int user_id);
+	// 좋아요 취소
+	int deleteUsersLikeCafe(Long users_id);
 
 }
