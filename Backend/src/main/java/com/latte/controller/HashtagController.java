6304869d354d2a4fbe8fda9247d81e27d6eff061
jinwon Hyun <94epicenter@gmail.com>
2020-02-13 17:06:54 +0900

    jw | modify minor err

--- Backend/src/main/java/com/latte/controller/HashtagController.java
+++ Backend/src/main/java/com/latte/controller/HashtagController.java
@@ -1,7 +1,6 @@
 package com.latte.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,7 +21,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.latte.model.Hashtag;
