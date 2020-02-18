a38ed7214f93eedf3a6646d13b941f2f9f2595f3
SiHo Ryu <ujakifox2@gmail.com>
2020-02-18 15:13:44 +0900

    SH | HomeController edited

--- Backend/src/main/java/com/latte/controller/HomeController.java
+++ Backend/src/main/java/com/latte/controller/HomeController.java
@@ -1,15 +1,25 @@
 package com.latte.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/v1")
-public class HomeController {
+//@RequestMapping("/v1")
+public class HomeController implements ErrorController{
 	@GetMapping("/")
 	public String index() {
-//		return "index.html";
-		return null;
+		return "index.html";
+	}
+
+	@GetMapping("/error")
+	public String redirect() {
+		return "index.html";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
 	}
 }
