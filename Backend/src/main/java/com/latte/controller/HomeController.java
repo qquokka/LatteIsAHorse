84d168e071cc81359dbcf9fb8f9694ec4be7bfda
jinwon Hyun <94epicenter@gmail.com>
2020-02-18 16:14:47 +0900

    jw | server redirect , modify minor error

--- Backend/src/main/java/com/latte/controller/HomeController.java
+++ Backend/src/main/java/com/latte/controller/HomeController.java
@@ -1,15 +1,19 @@
 package com.latte.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/v1")
-public class HomeController {
-	@GetMapping("/")
+public class HomeController implements ErrorController {
+	@GetMapping({ "/", "/error" })
 	public String index() {
-//		return "index.html";
-		return null;
+		return "index.html";
+//		return null;
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
 	}
 }
