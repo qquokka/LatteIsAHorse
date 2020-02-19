b0aa2a669dbee1949046f1e07d45ace940d2a70c
SiHo Ryu <ujakifox2@gmail.com>
2020-02-19 19:34:00 +0900

    SH | 회원탈퇴처리된 경우 로그인 못하게 구현함

--- Backend/src/main/java/com/latte/service/ICouponService.java
+++ Backend/src/main/java/com/latte/service/ICouponService.java
@@ -7,10 +7,10 @@ import com.latte.payload.CouponUseRequest;
 import com.latte.payload.CouponUseResponse;
 
 public interface ICouponService {
-	// DB 에 저장된 모든 쿠폰을 반환[관리자용]
+	// DB에 저장된 모든 쿠폰을 반환[관리자용]
 	List<Coupon> getCouponList();
 
-	// 카페아이디로 등록된 쿠폰을 보여줍니다.
+	// 카페 ID로 등록된 쿠폰을 보여줍니다.
 	List<Coupon> getCouponListByCafeId(Long cafe_id);
 
 	// Creat
@@ -27,6 +27,7 @@ public interface ICouponService {
 
 	int isExist(Coupon coupon);
 
+	// 쿠폰 사용 요청
 	int useCoupon(Coupon coupon);
 
 	int getCurrentCouponCount(Coupon coupon);
