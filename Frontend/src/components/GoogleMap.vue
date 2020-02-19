79045b2213a10b6ec08770be4c28a7b3556ab24e
dodonmountain <lkh151515@gmail.com>
2020-02-20 01:03:45 +0900

    prettier

--- Frontend/src/components/GoogleMap.vue
+++ Frontend/src/components/GoogleMap.vue
@@ -51,17 +51,17 @@
           }
         }"
       >
-      <div class="container pl-0">
-        <h5>{{ cafe.cafe_name }}</h5>
-        <small class="m-0">{{ cafe.cafe_address }}</small>
-        <br>
-                <small class="mt-3 font-weight-bold">
-          <fa icon="phone-alt" />
-          {{ cafe.cafe_phone }}
-        </small>
-                <div class="d-block d-md-none border shadow"> <router-link :to="`/cafe/${cafe.cafe_id}`">자세히 보기</router-link> </div>
-
-
+        <div class="container pl-0">
+          <h5>{{ cafe.cafe_name }}</h5>
+          <small class="m-0">{{ cafe.cafe_address }}</small>
+          <br />
+          <small class="mt-3 font-weight-bold">
+            <fa icon="phone-alt" />
+            {{ cafe.cafe_phone }}
+          </small>
+          <div class="d-block d-md-none border shadow">
+            <router-link :to="`/cafe/${cafe.cafe_id}`">자세히 보기</router-link>
+          </div>
         </div>
       </gmap-info-window>
     </gmap-map>
@@ -71,7 +71,7 @@
 <script>
 import Loading from "vue-loading-overlay";
 import "vue-loading-overlay/dist/vue-loading.css";
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "GoogleMap",
@@ -79,7 +79,8 @@ export default {
     Loading
   },
   props: {
-    prop_center: { //지도의 중심 위치
+    prop_center: {
+      //지도의 중심 위치
       type: Object,
       required: true
     },
@@ -100,35 +101,37 @@ export default {
     };
   },
   beforeMount() {
-    this.avheight = Math.min(window.innerHeight - 52.78, window.innerWidth * 1.3);
-    for (var lc=0;lc<1000;lc++) {
+    this.avheight = Math.min(
+      window.innerHeight - 52.78,
+      window.innerWidth * 1.3
+    );
+    for (var lc = 0; lc < 1000; lc++) {
       this.window_open.push(false);
     }
   },
   methods: {
     infoWindow(id) {
-      this.window_open.splice(id, 1, !this.window_open[id])
+      this.window_open.splice(id, 1, !this.window_open[id]);
       this.selected_cafe = this.filteredCafes[id];
       this.$emit("cafe_change_event", this.selected_cafe);
     },
-    getHashtagsByCafeIds(cafe_ids){
-      
-      console.log("cafe_ids : " + cafe_ids)
-      console.log(typeof cafe_ids)
+    getHashtagsByCafeIds(cafe_ids) {
+      console.log("cafe_ids : " + cafe_ids);
+      console.log(typeof cafe_ids);
       axios
         .post(`${this.$store.state.constants.SERVER}/map/hashtags`, {
           cafe_ids: cafe_ids
         })
         .then(res => {
-          if(res.status === 200){
-            console.log(res.data)
-            this.hashtags = res.data.map_hashtags
-            this.$emit("hashtag_get_event", this.hashtags)
-            console.log(this.hashtags)
+          if (res.status === 200) {
+            console.log(res.data);
+            this.hashtags = res.data.map_hashtags;
+            this.$emit("hashtag_get_event", this.hashtags);
+            console.log(this.hashtags);
           }
         })
         .catch(err => {
-          console.log(err)
+          console.log(err);
         });
     },
     geolocate() {
@@ -139,13 +142,17 @@ export default {
           level: 12
         })
         .then(res => {
-          console.log(res.data)
-          this.cafes = res.data.filter(ccc => ccc.cafe_name.includes(this.filtername));
+          console.log(res.data);
+          this.cafes = res.data.filter(ccc =>
+            ccc.cafe_name.includes(this.filtername)
+          );
           //cafes 리스트에서 cafe_id만 추출하여 새로운 리스트 만듬
-          const cafe_ids = res.data.map((cafe) => { return cafe.cafe_id})
+          const cafe_ids = res.data.map(cafe => {
+            return cafe.cafe_id;
+          });
           //카페 ID로 해시태그 가져오기
-          this.getHashtagsByCafeIds(cafe_ids)
-          this.$forceUpdate()
+          this.getHashtagsByCafeIds(cafe_ids);
+          this.$forceUpdate();
           setTimeout(() => {
             this.mapLoading = false;
           }, 1);
@@ -157,8 +164,8 @@ export default {
   },
   computed: {
     filteredCafes() {
-        return this.$store.getters.filteredCafes(this.filtername)
-      },
+      return this.$store.getters.filteredCafes(this.filtername);
+    }
   }
 };
 </script>
